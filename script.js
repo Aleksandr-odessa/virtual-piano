12b6ab5ac1711b1b909449ae4615bce5619a351f
Aleksandr-odessa <77878638+Aleksandr-odessa@users.noreply.github.com>
2021-04-05 14:24:13 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let elem = document.querySelectorAll('.piano-key');
+elem.forEach(elem => {
+    elem.style.backgroundColor = 'yellow';
+});
+let elem_sharp = document.querySelectorAll('.sharp');
+elem_sharp.forEach(query => {
+    query.style.backgroundColor = 'red';
+});
